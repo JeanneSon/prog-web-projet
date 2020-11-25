306363a73dab7e35a91ef9dd4b89674f5c1bdbe4
Michelle <43150339+mmielk19@users.noreply.github.com>
2020-11-25 11:41:01 +0100

    Update dropdown.php

--- dropdown.php
+++ dropdown.php
@@ -6,6 +6,7 @@
     </head>
     <body>
         <?php include 'Donnees.inc.php';?>
+        sous-catégorie und super-catégorie
         <form>
           <select>
                 <option selected="selected">Choose one</option>
@@ -13,16 +14,23 @@
                 foreach($Hierarchie as $h => $g){
                     foreach($g as $item => $value){
                         foreach($value as $i => $v){
-                            echo "<option value='strtolower($v)'>$v</option>";}
+                            echo "<option>$v</option>";}
                     }
                 }
                 ?>
             </select>
             <input type="submit" value="Submit">
         </form>
+        super-categorie
         <form>
             <select>
                 <option selected="selected">Choose one</option>
+                <?php
+                    foreach($Hierarchie as $name) { 
+                      echo "<option >$name</option>";
+                  
+                    } ?>
+                
                 
             </select>
         </form>
