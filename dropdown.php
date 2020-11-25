1396d2c92f2801e7644f01c86d46136665f48348
Michelle <43150339+mmielk19@users.noreply.github.com>
2020-11-25 11:13:13 +0100

    Update dropdown.php

--- dropdown.php
+++ dropdown.php
@@ -1,25 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Dynamically Generate Select Dropdowns</title>
-</head>
-<body>
-    <?php include 'Donnees.inc.php';
-    ?>
-<form>
-  <select>
-        <option selected="selected">Choose one</option>
-        <?php
-        foreach($Hierarchie as $h => $g){
-            foreach($g as $item => $value){
-                foreach($value as $i => $v){
-                    echo "<option value='strtolower($v)'>$item</option>";}
-            }
-        }
-        ?>
-    </select>
-    <input type="submit" value="Submit">
-</form>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title>Dynamically Generate Select Dropdowns</title>
+    </head>
+    <body>
+        <?php include 'Donnees.inc.php';?>
+        <form>
+          <select>
+                <option selected="selected">Choose one</option>
+                <?php
+                foreach($Hierarchie as $h => $g){
+                    foreach($g as $item => $value){
+                        foreach($value as $i => $v){
+                            echo "<option value='strtolower($v)'>$v</option>";}
+                    }
+                }
+                ?>
+            </select>
+            <input type="submit" value="Submit">
+        </form>
+        <form>
+            <select>
+                <option selected="selected">Choose one</option>
+                
+            </select>
+        </form>
+        <form>
+            <select>
+                <option selected="selected">Choose one</option>
+            </select>
+        </form>
+    </body>
 </html>
