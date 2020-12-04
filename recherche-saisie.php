e784fc8acf734f72e41f46a71e9905db80d4f700
Aurianne <43066686+aurrriianne@users.noreply.github.com>
2020-12-04 20:20:28 +0100

    update invalide ingredient

--- recherche-saisie.php
+++ recherche-saisie.php
@@ -14,23 +14,38 @@
 <div id="elementsTrouves">
     <?php
     include_once("getIngredients.php");
+    $mauvaisIngredient = false;
     echo '<ul>';
     if ( (isset($_POST['autocomplete-1'])) && ((trim($_POST['autocomplete-1'])) != "") ){
     	$ingredient = trim($_POST['autocomplete-1']);
-    	$recettesCorrespondants = recettesCorrespondants($ingredient);
-    	if (isset($_POST['autocomplete-2'])) {
-    		$nonIngredient = trim($_POST['autocomplete-2']);
-    		$recettesNonCorrespondantes = recettesCorrespondants($nonIngredient);
-    		$recettefinales = array_diff($recettesCorrespondants, $recettesNonCorrespondantes);
-    	} else {
-            $recettefinales = $recettesCorrespondants;
-        } 
-    }
+        //if (rechercheIngredient($ingredient)) {
+    	   $recettesCorrespondants = recettesCorrespondants($ingredient);
+    	   if ( (isset($_POST['autocomplete-2'])) && ((trim($_POST['autocomplete-2'])) != "") ){
+    		  $nonIngredient = trim($_POST['autocomplete-2']);
+                if (rechercheIngredient($nonIngredient)) {
+    		        $recettesNonCorrespondantes = recettesCorrespondants($nonIngredient);
+    		        $recettefinales = array_diff($recettesCorrespondants, $recettesNonCorrespondantes);
+                } else {
+                    $mauvaisIngredient = true;
+                    echo $nonIngredient." n'est pas un ingrédient valide. Veuillez en selectionner un nouveau. <br>";
+                }
+    	    } else {
+                $recettefinales = $recettesCorrespondants;
+            } if (!rechercheIngredient($ingredient)) {
+                $mauvaisIngredient = true;
+                echo $ingredient." n'est pas un ingrédient valide. Veuillez en selectionner un nouveau. <br>";
+            }
+    } 
     else if ( (isset($_POST['autocomplete-2'])) && ((trim($_POST['autocomplete-2'])) != "") ){
     	$nonIngredient = trim($_POST['autocomplete-2']);
-    	$allRecettes = recettesCorrespondants('Aliment');
-    	$recettesNonCorrespondantes = recettesCorrespondants($nonIngredient);
-    	$recettefinales = array_diff($allRecettes, $recettesNonCorrespondantes);
+        if (rechercheIngredient($nonIngredient)) {
+    	   $allRecettes = recettesCorrespondants('Aliment');
+    	   $recettesNonCorrespondantes = recettesCorrespondants($nonIngredient);
+    	   $recettefinales = array_diff($allRecettes, $recettesNonCorrespondantes);
+        } else {
+            $mauvaisIngredient = true;
+            echo $nonIngredient." n'est pas un ingrédient valide. Veuillez en selectionner un nouveau.";
+        }
     }
     if (!empty($recettefinales)) {
     	echo 'Voici les recettes que nous vous proposons: <br>';
@@ -38,7 +53,7 @@
         	echo '<li><a href="?page=cocktail-detail&cocktailId='.$index.'"">'.$Recettes[$index]["titre"].'</a></li>';
     	}
     } else {
-        if ( (isset($_POST['autocomplete-1'])) || (isset($_POST['autocomplete-2'])) ){
+        if ( ( (isset($_POST['autocomplete-1'])) || (isset($_POST['autocomplete-2'])) ) && ($mauvaisIngredient == false) ){
     	    echo 'Malheureusement aucune recette ne correspond à votre demande, nous vous proposons celles ci: <br>';
     	    if ((trim($_POST['autocomplete-1'])) != ""){
     	       echo 'Avec '.$ingredient;
